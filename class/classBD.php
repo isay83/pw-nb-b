884f202043521eae827cf22b1f3f62f5fff51306
isay83 <jisay.2003@gmail.com>
2024-03-19 10:05:16 -0600

    add class structure for classBD

---
+++ class/classBD.php
@@ -0,0 +1,14 @@
+<?php
+class BaseDatos
+{
+    var $a_servidor;
+    var $a_user;
+    var $a_pwd;
+    var $a_baseDatos;
+    var $a_bloqRegistros;
+    var $a_numeRegistros;
+
+    function __construct()
+    {
+    }
+}
