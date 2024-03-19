4818677715eff89e750ebd9944d2a2bd83354786
isay83 <jisay.2003@gmail.com>
2024-03-19 09:40:28 -0600

    add class folder

--- validar.php
+++ validar.php
@@ -17,14 +17,27 @@ else {
     $datosComprimidos = mysqli_query($conexion, $query);
 
     // procesar los datos consultados
-    while ($row = mysqli_fetch_assoc($datosComprimidos)) {
+
+    /*
+        $datos = mysqli_fetch_object($datosComprimidos); regresa un objeto
+            echo $datos->Correo
+
+        $datos = mysqli_fetch_array($datosComprimidos);
+            echo $datos[0]
+            echo $datos['Correo'] - arreglos asociativos con tamaño del doble de los campos
+        mysqli_fetch($datosComprimidos);
+    */
+    if ($row = mysqli_fetch_assoc($datosComprimidos)) { //fetch accede al elemento y muestra su contenido
         echo "Nombre: " . $row['Nombre'] . "<br>";
         echo "Email: " . $row['Email'] . "<br>";
     }
 
+    if (mysqli_num_rows($datosComprimidos) == 1) {
+    } else {
+        header('location: html/login.php?m=1'); // redireccionar hacia un lugar en particular
+    }
+
+
     // 3er paso
     mysqli_close($conexion);
 }
-
-?>
-```
